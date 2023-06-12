8ad7686606ffda36c792abafd5d52a2954365ffd
Julian Katz <juliankatz@gmx.de>
2023-06-12 18:31:19 +0200

    change base Url

--- src/axiosURL.js
+++ src/axiosURL.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
-const instance = axios.create({ baseURL: "http://localhost:3020/" })
+const instance = axios.create({ 
+    baseURL: "http://localhost:3015/",
+    withCredentials: true
+})
+
+
 
 export default instance;
