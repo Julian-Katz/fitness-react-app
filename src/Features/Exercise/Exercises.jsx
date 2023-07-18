089b90fbe54328117d11c9cece881ff270fd499d
Julian Katz <juliankatz@gmx.de>
2023-07-18 22:32:26 +0200

    display exercises

--- src/Features/Exercise/Exercises.jsx
+++ src/Features/Exercise/Exercises.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Exercises.scoped.css';
 import AddExercises from './Components/AddExercises';
+import { useSelector, useDispatch } from 'react-redux';
+import { loadAsync, selectExercises } from './exerciseSlice';
 
 function Exercises() {
-    return (
-        <div className='content-area'>
-            <h1>Übungen</h1>
-            <AddExercises />
-        </div>
-    );
+  const dispatch = useDispatch();
+  const exercises = useSelector(selectExercises);
+
+  useEffect(() => {
+    dispatch(loadAsync());
+  }, [dispatch]);
+
+  return (
+    <div className='content-area'>
+      <h1>Übungen</h1>
+      <AddExercises />
+      {
+        exercises.map((exercise) => (
+          <div>{exercise.name}</div>
+        ))
+      }
+    </div>
+  );
 };
 
 export default Exercises;
